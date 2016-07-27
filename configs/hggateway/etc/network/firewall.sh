18d645b80ea36bb41aa924a17d1f3abe0999d334
Georges Toth <georges@trypill.org>
2016-07-27 16:47:04 +0200

    fix wrong IPv6; add DSL interface

--- configs/hggateway/etc/network/firewall.sh
+++ configs/hggateway/etc/network/firewall.sh
@@ -26,10 +26,13 @@ iptables -A INPUT -i eth0.161 -d 10.17.0.0/24 -j DROP
 
 # INPUT
 iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -d 224.0.0.1 -j DROP
 iptables -A INPUT -m state --state INVALID -j DROP
 iptables -A INPUT -m state --state ESTABLISHED -j ACCEPT
-iptables -A INPUT -p ospf -j ACCEPT
+
+iptables -A INPUT -i eth2 -p ospf -j ACCEPT
+iptables -A INPUT -i tap0 -p ospf -j ACCEPT
+iptables -A INPUT -i tap1 -p ospf -j ACCEPT
+
 iptables -A INPUT -p icmp -m icmp --icmp-type 3 -j ACCEPT
 iptables -A INPUT -p icmp -m icmp --icmp-type 4 -j ACCEPT
 iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
@@ -43,6 +46,9 @@ iptables -A INPUT -p tcp -m tcp --dport 123 -j ACCEPT
 iptables -A INPUT -p udp -m udp --dport 123 -j ACCEPT
 iptables -A INPUT -i eth0.141 -j ACCEPT
 #iptables -A INPUT -i eth0.171 -j ACCEPT #test
+
+#iptables -A INPUT -j LOG
+iptables -A INPUT -d 224.0.0.1 -j DROP
 
 
 #OUTPUT
@@ -69,6 +75,7 @@ iptables -A FORWARD -d 31.22.122.2/32 -i eth0.161 -j ACCEPT
 iptables -A FORWARD -d 31.22.122.2/32 -i eth0.171 -j ACCEPT
 iptables -A FORWARD -d 10.14.1.10/32 -i eth0.171 -j ACCEPT
 iptables -A FORWARD -d 192.168.1.0/24 -i eth0.141 -j ACCEPT
+iptables -A FORWARD -d 192.168.178.9/24 -i eth0.141 -j ACCEPT
 
 iptables -A FORWARD -d 10.14.1.3 -p tcp --dport 80 -j ACCEPT  # iving
 iptables -A FORWARD -d 10.14.1.3 -p tcp --dport 443 -j ACCEPT  # iving
@@ -87,6 +94,8 @@ iptables -t nat -A POSTROUTING -o eth0.141 -d 10.14.1.3 -j SNAT --to-source 10.1
 iptables -t nat -A POSTROUTING ! -s 31.22.122.0/23 ! -d 192.168.1.0/24 -o tap+ -j MASQUERADE
 iptables -t nat -A POSTROUTING -d 31.22.121.90/32 -o eth1 -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 10.14.1.0/24 -d 192.168.1.0/24 -o eth2 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.14.1.0/24 -d 192.168.178.0/24 -o eth1 -j MASQUERADE
+
 
 # wondershaper
 #wondershaper tap0 95000 95000
